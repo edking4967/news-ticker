6156f87276c31308c1df6d9b2b32003254e26681
Edward King <edwardking@Edwards-MacBook-Air.local>
2026-02-15 19:42:55 -0700

    seems to all work served from flask

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,25 +1,22 @@
-import json, requests, feedparser
-from flask import Flask
-from flask_cors import CORS
+from flask import send_from_directory, Flask
+import requests, feedparser
 
 app = Flask(__name__)
 
-CORS(app)   # allow all origins (dev only)
+
+@app.route("/")
+def serve():
+    return send_from_directory("dist", "index.html")
+
+@app.route("/assets/<path:path>")
+def serve_assets(path):
+    return send_from_directory("dist/assets", path)
 
 @app.route("/feed")
 def feed():
     url = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
-    response = requests.get(url, timeout=5) # Takes a long time 
+    response = requests.get(url, timeout=5) # Takes a long time
     response.raise_for_status()  # raises error if download failed
     feed = feedparser.parse(response.content)
     return feed
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-# TODO: create a route like def parseXml(xmlUrl):
-# return root
-# (Would this be safe?) NO. cannot fetch/parse arbitrary xml.
-# Safer way?# 
-# TODO: go through feed and make json object to pass to react
--- flask-backend/test.py
+++
@@ -1,7 +0,0 @@
-import json, feedparser
-from flask import Flask
-from parse import getRss, getXml, getJson
-import xml.etree.ElementTree as ET
-
-r = getRss("https://rss.nytimes.com/services/xml/rss/nyt/World.xml")
-feed = feedparser.parse(r.content)
